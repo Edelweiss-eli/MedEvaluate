f50b32a8cb696f23fe00a63f410a42eb58a1b56c
Edelweiss-eli <enderspark101@gmail.com>
2025-02-07 21:38:10 +0800

    Added map func
    take note of cache

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import folium
+import os
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.window import Window
 from kivy.graphics import Color, Rectangle
-
+from kivy_garden.mapview import MapView, MapMarker
 
 class HomeScreen(Screen):
     pass
@@ -17,11 +19,17 @@ class SearchArticlesScreen(Screen):
 class WeGamingScreen(Screen):
     pass
 
-class TestApp(App):
+class MapScreen(Screen):
+    def on_enter(self):
+        map_view = self.ids.map_view
+        marker = MapMarker(lat=14.8527, lon=120.8160)
 
+        map_view.add_marker(marker)
+
+class TestApp(App):
     def build(self):
         Window.size = (360, 800)
-        return
+        return Builder.load_file("test.kv")
 
 if __name__ == '__main__':
     TestApp().run()
